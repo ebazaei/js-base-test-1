bc25e5dadebbde5846da790939680bac5fa2da7e
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-16 11:56:40 +0100

    js-base-1

---
+++ base.js
@@ -0,0 +1,41 @@
+/*var esm = "ehsan";
+var lastname = "bazaei";*/
+//console.log(esm + " " + lastname);
+
+// let age =  30;
+// age=20
+// console.log(age)
+
+// const isAdmin = 678
+// console.log(isAdmin)
+// console.log(typeof(isAdmin))
+
+// let object_1 = {
+//     firstname : "Ehsan",
+//     lastname : "Bazaei",
+//     age : 41,
+//     isAdmin : true,
+//     hobby : ["Footbal","Art",30,true,"",]
+// }
+// console.log(object_1);
+// console.log(object_1.hobby[3]);
+// console.log(typeof(object_1.hobby[3]));
+// console.log(object_1["hobby"].length)
+
+// function ehsan1(first, last){
+//     console.log("hoi" + " " + " " + "");
+//     console.log(`Hoi ${first} ${last}`);
+// }
+// ehsan1('ehsan','bazaei')
+
+
+// function ehsan2(first, last){
+//      return "Hallo" + " " + first + " " + last;
+// }
+// console.log(ehsan2("ehsan", "bazaei"))
+
+
+// function addNumber(num1, num2){
+//     return (num1 + num2 + num1) / 2;
+// }
+// console.log(addNumber(2,3));
