d3c822b49d29dced8692b36b497152201cc63345
ehsan bazaei <bazaeiehsan@gmail.com>
2021-12-17 12:08:29 +0100

    while loop

--- base.js
+++ base.js
@@ -35,7 +35,131 @@ var lastname = "bazaei";*/
 // console.log(ehsan2("ehsan", "bazaei"))
 
 
+
+// function ehsan3(first, last){
+//     console.log("salam" + " " + first + " " + last);
+// }
+// let firstn = "Esi"
+// let lastn = "bzi"
+// ehsan3(firstn, lastn);
+
 // function addNumber(num1, num2){
 //     return (num1 + num2 + num1) / 2;
 // }
 // console.log(addNumber(2,3));
+
+
+
+// let object_2 = {
+// firstname : "Ehsan",
+// lastname : "Bazaei",
+// age : 41,
+// isAdmin : true,
+// hobby : ["Footbal","Art",30,true,"",]
+// }
+
+// console.log(object_2.hobby[1]);
+
+// object_2.hobby[1] = "kunst";
+// console.log(object_2.hobby[1]);
+
+// console.log(object_2["hobby"]["1"])
+
+// let x = 5
+// let y = 2
+// let z = ++y
+
+// let a = 1
+// let b = a++
+// let c = ++b
+
+// console.log(c)
+
+// let num1 = 10
+// console.log(num1++)
+// console.log(num1++)
+// console.log(++num1)
+
+// let num1 = 10;
+// let name1 = '10';
+// console.log(num1 == name1 && num1 === name1)
+// console.log(num1 == name1 || num1 === name1)
+
+// Truthy falsy
+// function getcolor(color){
+//     const mycolor = color || 'red';
+//     console.log(mycolor);
+// }
+// getcolor('blue');
+
+// let myAge = 41;
+
+// if(myAge <= 25){
+//     console.log('i am young')
+// }else if(myAge <=50){
+//     console.log('between 25 tot 40')
+// }else{
+//     console.log('i am old')
+// }
+
+
+// let num = 15
+// function mohasebeh1(num){
+//     if ( num % 2 === 0 ){
+//         console.log(num + ' is ' + 'adade zouj')
+//     }else{
+//         console.log(num + ' is ' + 'adade fard')
+//     }
+// }
+// mohasebeh1(num)
+// mohasebeh1(10)
+// mohasebeh1(12+3)
+
+
+// let num1 = 11;
+// function sumnum(num1, num2){
+//     console.log(num1+num2)
+// }
+// num1 = num1 + sumnum(2,2);
+
+
+// function mohasebeh2 (nm1,nm2){
+//     return(nm1+nm2)
+// }
+// console.log(mohasebeh2(5,4))
+
+
+
+// let list = [1 , 2 , 5 ,'t', 5 ];
+// console.log(list)
+// console.log(list.length)
+
+// list.push(75,5,6)
+// console.log(list)
+
+// list.pop();
+// console.log(list)
+
+
+//let list2 = [1,3,5,7,9];
+
+// for(let i=1; i < 10; i++){
+//     console.log('my number is: ' + i);
+//     if(i % 2 == 0){
+//         console.log('and this number is : Even')
+//     }else{
+//         console.log('and this number is : Odd')
+//     }
+//     console.log('----------------------');
+// }
+
+
+
+let i = 0;
+while (i < 10) {
+  console.log('my number is: ' + i);
+  console.log('-----------------')
+  i++
+}
+
+
